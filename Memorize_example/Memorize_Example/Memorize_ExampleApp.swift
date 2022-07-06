b3182aed3cda66b250b7aaa17e54aee19eef3c9d
GerardoGarzon <ggonzalezg1300@alumno.ipn.mx>
2022-07-06 12:30:03 -0500

    Memorize first example

---
+++ Memorize_example/Memorize_Example/Memorize_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_ExampleApp.swift
+//  Memorize_Example
+//
+//  Created by Gerardo Garzon on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
